b7fdd7693f870a9aff61563301a4d2da4e22f01f
Chinmay <chinmay.kabi@live.com>
2021-06-13 02:42:24 +0530

    feat: Removed callkeep, added notification
    Callkeep is not behaving as expected, so using flutter local
    notification as a makeshift solution.
    With callkeep plugin, the caller UI freezes when the call is accepted.
    From the logs it seems like the call is placed more than once on the
    plugin side, and hence the issue. Needs further investigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:faller/creds.dart';
 import 'package:faller/utils/fcm_push.dart';
@@ -5,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -24,12 +28,16 @@ class _FallerAppState extends State<FallerApp> {
   bool _switch = false;
   var _messaging;
   String message = 'Please chat';
+  bool speakerPhone = false;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
   }
+
+  RtcEngineConfig config = RtcEngineConfig(Credentials.APP_ID);
+  var engine;
 
   // Init the app
   Future<void> initPlatformState() async {
@@ -38,16 +46,13 @@ class _FallerAppState extends State<FallerApp> {
     await Firebase.initializeApp();
     _messaging = FirebaseMessaging.instance;
     _messaging.subscribeToTopic('faller');
-    FirebaseMessaging.onMessage.listen((event) {
+    FirebaseMessaging.onMessage.listen((event) async {
       setState(() {
         message = "Notification received";
       });
     });
-
-    // Create RTC client instance
-    RtcEngineConfig config = RtcEngineConfig(Credentials.APP_ID);
-
-    var engine = await RtcEngine.createWithConfig(config);
+    FirebaseMessaging.onBackgroundMessage(myBackgroundMessageHandler);
+    engine = await RtcEngine.createWithConfig(config);
     // Define event handling logic
     engine.setEventHandler(RtcEngineEventHandler(
         joinChannelSuccess: (String channel, int uid, int elapsed) {
@@ -71,6 +76,13 @@ class _FallerAppState extends State<FallerApp> {
     engine.setEnableSpeakerphone(true);
   }
 
+  toggleSpeaker() {
+    setState(() {
+      speakerPhone = !speakerPhone;
+    });
+    engine.setEnableSpeakerphone(speakerPhone);
+  }
+
   // Build chat UI
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -91,9 +103,50 @@ class _FallerAppState extends State<FallerApp> {
           title: Text('Agora Audio quickstart'),
         ),
         body: Center(
-          child: Text(message),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              TextButton(
+                  onPressed: () => toggleSpeaker(),
+                  child: Text('Speaker is on- $speakerPhone')),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Future<dynamic> myBackgroundMessageHandler(RemoteMessage event) async {
+  print("Received");
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('ic_launcher');
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  const int insistentFlag = 4;
+  final AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+          'your channel id', 'your channel name', 'your channel description',
+          importance: Importance.max,
+          priority: Priority.high,
+          ticker: 'ticker',
+          autoCancel: false,
+          playSound: true,
+          fullScreenIntent: true,
+          additionalFlags: Int32List.fromList(<int>[insistentFlag]));
+  final NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics);
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    'insistent title',
+    'insistent body',
+    platformChannelSpecifics,
+    payload: 'item x',
+  );
 }
